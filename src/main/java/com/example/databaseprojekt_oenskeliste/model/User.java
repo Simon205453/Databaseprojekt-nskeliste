9c4733d8271ef89aa6137b09087fd396c728b0a7
Simon <simonffsss@gmail.com>
2022-03-28 11:00:17 +0200

    gitinit

--- src/main/java/com/example/databaseprojekt_oenskeliste/model/User.java
+++ src/main/java/com/example/databaseprojekt_oenskeliste/model/User.java
@@ -1,4 +1,20 @@
 package com.example.databaseprojekt_oenskeliste.model;
 
+import java.util.ArrayList;
+
 public class User {
+
+    private String username;
+    private String password;
+    private String userId;
+    private ArrayList<Wishes> wishlist;
+
+    public User(String username, String password, String userId, ArrayList<Wishes> wishlist) {
+        this.username = username;
+        this.password = password;
+        this.userId = userId;
+        this.wishlist = wishlist;
+    }
+
+
 }
--- src/main/java/com/example/databaseprojekt_oenskeliste/model/Wishes.java
+++ src/main/java/com/example/databaseprojekt_oenskeliste/model/Wishes.java
@@ -1,4 +1,14 @@
 package com.example.databaseprojekt_oenskeliste.model;
 
 public class Wishes {
+
+    private String wishname;
+    private String wishId;
+    private String price;
+
+    public Wishes(String wishname, String wishId, String price) {
+        this.wishname = wishname;
+        this.wishId = wishId;
+        this.price = price;
+    }
 }
