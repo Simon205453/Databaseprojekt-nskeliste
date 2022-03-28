ca122c91d173316a43cc0d8c19c0d5a54ee328f7
Simon <simonffsss@gmail.com>
2022-03-28 10:44:50 +0200

    gitinit

---
+++ src/main/java/com/example/databaseprojekt_oenskeliste/DatabaseprojektOenskelisteApplication.java
@@ -0,0 +1,13 @@
+package com.example.databaseprojekt_oenskeliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseprojektOenskelisteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseprojektOenskelisteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/databaseprojekt_oenskeliste/DatabaseprojektOenskelisteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databaseprojekt_oenskeliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseprojektOenskelisteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
